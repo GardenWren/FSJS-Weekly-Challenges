a6d4b75fb985bb3cbe6c6cbb8fa5506741933c33
Garden Paquin <garden.paquin@gmail.com>
2016-09-04 18:16:24 -0400

    corrected duplication in app.js

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,10 +1,15 @@
 //create function
-  //control and return statements
+  
   
 function HelloWorld(){
     var hello;
     while(true === true){
+    	
+//Prompt user for input and store variable
       hello = prompt('Enter Greeting!');
+      
+    //control and return statements
+	//Alert user results
       if (hello === 'Hello') {
          alert('Hello World!');
          return;
@@ -13,22 +18,10 @@ function HelloWorld(){
       }
     }
   }
- 
+  
+ //Call function
  HelloWorld();
 
-function HelloWorld(){
-  var hello
-  do{
-  	hello = prompt('Enter Greeting!')
-  } while (hello !== 'Hello');
-  alert("Hello World!");
-}
-
-HelloWorld();
 
 
-//Prompt user for input and store variable
 
-//Call function
-
-//Alert user results
