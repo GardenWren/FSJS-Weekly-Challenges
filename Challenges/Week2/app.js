9c246355415937c66ecde70ec60d3ad2098b7ee1
Garden Paquin <garden.paquin@gmail.com>
2016-09-04 17:51:11 -0400

    updated homework

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,5 +1,20 @@
 //create function
   //control and return statements
+  
+function HelloWorld(){
+    var hello;
+    while(true === true){
+      hello = prompt('Enter Greeting!');
+      if (hello === 'Hello') {
+         alert('Hello World!');
+         return;
+      }else{
+      	alert('Incorrect Greeting');
+      }
+    }
+  }
+ 
+ HelloWorld();
 
 //Prompt user for input and store variable
 
